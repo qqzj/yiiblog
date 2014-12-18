95213e38abc72ef8d115a2c0b1d9135347d869ff
wangchaojie <popxmz@Gmail.com>
2014-12-18 16:48:19 +0800

    验证码可以点击刷新

--- protected/modules/admin/views/index/index.php
+++ protected/modules/admin/views/index/index.php
@@ -4,7 +4,9 @@
 <?php $form = $this->beginWidget('CActiveForm');?>
 用户名:<?php echo $form->textField($loginForm, 'username', array('id'=>'user'));?><br />
 密&nbsp;&nbsp;&nbsp;码:<?php echo $form->passwordField($loginForm, 'password', array('id'=>'pass'));?><br />
-验证码:<?php echo $form->textField($loginForm, 'captcha', array('id'=>'captcha'));?><?php $this->widget('CCaptcha');?><br />
+验证码:<?php echo $form->textField($loginForm, 'captcha', array('id'=>'captcha'));?><!--<?php $this->widget('CCaptcha');?>-->
+<?php $this->widget('CCaptcha',array('showRefreshButton'=>false,'clickableImage'=>true,'imageOptions'=>array('alt'=>'点击换图','title'=>'点击换图','style'=>'cursor:pointer'))); ?>
+<br />
 记住我:<?php echo $form->checkBox($loginForm, 'rememberMe', array('id'=>'remember'));?><br />
 <input type="submit" value="登陆" /><hr />
 <?php $this->endWidget();?>
