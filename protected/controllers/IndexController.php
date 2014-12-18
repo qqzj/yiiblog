41ac6fb89c3ea93cae44f6efd17e135fe5e60618
wangchaojie <popxmz@Gmail.com>
2014-12-18 11:35:19 +0800

    局部修改默认布局

--- protected/controllers/IndexController.php
+++ protected/controllers/IndexController.php
@@ -1,6 +1,7 @@
 <?php
 	class IndexController extends Controller{
 		function actionIndex(){
+			$this->layout='//layouts/column3';
 			//$this->renderPartial('index', array('a'=>'apple', 'b'=>'banana'));
 			$this->render('index', array('a'=>'apple', 'b'=>'banana'));
 		}
